0476792ee98b6bc6927958f3b2d1a6100ef97487
fork52 <mrunankmistry52@gmail.com>
2021-05-10 19:59:21 +0530

    Added documentation and renamed methods in Graphs/cycle_detect.cpp.

--- Graphs/cycle_detect.cpp
+++ Graphs/cycle_detect.cpp
@@ -1,7 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-// https://trykv.medium.com/algorithms-on-graphs-directed-graphs-and-cycle-detection-3982dfbd11f5
-// https://binarysearch.com/problems/Course-Scheduling
+
+/*
+	References:
+		https://trykv.medium.com/algorithms-on-graphs-directed-graphs-and-cycle-detection-3982dfbd11f5
+
+	Related Problem: 
+		https://binarysearch.com/problems/Course-Scheduling
+
+*/
+
+
+/*
+	Assumptions:
+		- Adjacency list is of size n where n is the number of nodes.
+*/
+template<typename T1>
 struct Graph
 {
 	enum node_status
@@ -10,14 +24,26 @@ struct Graph
 		explore = 1,
 		visited = 2
 	};
-	vector<vector<int>> graph;
+
+	vector<vector<T1>> graph;
 	vector<node_status> status;
-	Graph(vector<vector<int>> &adj_list)
+	
+	/*
+		Initializes the graph with adjacency list passed.
+		Intitializes the status array to unvisited for all nodes.
+		Time: O(n)   
+		Space: O(n)
+	*/
+	Graph(vector<vector<T1>> &adj_list)
 	{
 		graph = adj_list;
 		status.assign(adj_list.size(), unvisited);
 	}
-	bool dfs(int node)
+
+	/*
+		Performs DFS on the node to check back-edges.
+	*/
+	bool has_back_edge(int node)
 	{
 		if (status[node] == visited)
 			return false;
@@ -25,21 +51,28 @@ struct Graph
 			return true;
 		status[node] = explore;
 		for (auto const &neigh : graph[node])
-			if (dfs(neigh))
+			if (has_back_edge(neigh))
 				return true;
 		status[node] = visited;
 		return false;
 	}
-	bool check_cycle()
+
+	/*
+		Returns whether the graph is cyclic.
+	*/
+	bool is_cyclic()
 	{
-		for (int i = 0; i < graph.size(); i++)
-			if (dfs(i))
+		for (int node = 0; node < graph.size(); node++)
+			if(has_back_edge(node))
 				return true;
 		return false;
 	}
 };
-bool solve(vector<vector<int>> &courses)
-{
-	auto g = Graph(courses);
-	return !g.check_cycle();
+
+
+
+int main(){
+	vector< vector<int> > adj = {{0}};
+	Graph<int> g(adj);
+	cout << g.is_cyclic();
 }
