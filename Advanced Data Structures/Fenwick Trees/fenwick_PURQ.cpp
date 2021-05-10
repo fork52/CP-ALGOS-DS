4208fc1019685fe3bc1120b31d5dfb4d43d2af1f
decimalpack <decimalpack@users.noreply.github.com>
2021-05-10 17:38:04 +0530

    Add 0 index, arbitrary range, and custom operation
    Added two methods to support 0 indexing
    - point_update_0indexed
    - range_query_0indexed
    
    Added two functions f and f_rev to easily customize the operation
    
    Added range_query(l,r) and range_query_0indexed(l,r) to query arbitrary ranges

--- Advanced Data Structures/Fenwick Trees/fenwick_PURQ.cpp
+++ Advanced Data Structures/Fenwick Trees/fenwick_PURQ.cpp
@@ -13,7 +13,6 @@ References:
     3) CP-algos: https://cp-algorithms.com/data_structures/fenwick.html
 */
 
-
 /*
     Implementation of Fenwick Tree supporting:
     Point Increments/Updates and Range Queries.
@@ -24,55 +23,92 @@ References:
     The fenwick array is 1-indexed unlike the orgininal array.
     Please keep this in mind when you query!
 */
-template< typename T1>
-class Fenwick_Tree{
+template <typename T1>
+class Fenwick_Tree
+{
 public:
     int n;
     vector<T1> fenwick;
 
+private:
+    inline T1 f(T1 x, T1 y)
+    {
+        return x + y;
+    }
+    inline T1 f_rev(T1 x, T1 y)
+    {
+        return x - y;
+    }
+
+public:
     // Empty Constructor
-    Fenwick_Tree(){}
+    Fenwick_Tree() {}
 
     /* Constructs a Fenwick Tree for the given array*/
-    Fenwick_Tree(vector<T1> &arr){
+    Fenwick_Tree(vector<T1> &arr)
+    {
         n = arr.size() + 1;
-        fenwick = vector<T1>( n , 0 );
+        fenwick = vector<T1>(n, 0);
 
         copy(arr.begin(), arr.end(), fenwick.begin() + 1);
 
         int parent_ind;
-        for(int i = 1; i < n; i++){
+        for (int i = 1; i < n; i++)
+        {
             parent_ind = i + (i & -i);
-            if(parent_ind < n){
-                fenwick[parent_ind] += fenwick[i];
+            if (parent_ind < n)
+            {
+                fenwick[parent_ind] = f(fenwick[parent_ind], fenwick[i]);
             }
         }
     }
 
-
     /*Returns the sum of elements from 1....i in arr*/
-    int range_query(int i){
+    T1 range_query(int i)
+    {
         T1 total = 0;
-        while( i > 0){
+        while (i > 0)
+        {
             total += fenwick[i];
-            i -= i & -i;   //flip last set bit
+            total = f(total, fenwick[i]);
+            i -= i & -i; //flip last set bit
         }
         return total;
     }
 
+    T1 range_query(int l, int r)
+    {
+        return f_rev(range_query(r), range_query(l - 1));
+    }
+
+    T1 range_query_0indexed(int i)
+    {
+        return range_query(i + 1);
+    }
+
+    T1 range_query_0indexed(int l, int r)
+    {
+        return f_rev(range_query_0indexed(r + 1), range_query_0indexed(l));
+    }
     /* Add certain value 'x' to index i */
-    void point_update(int i, T1 x){
-        while(i < n){
-            fenwick[i] += x;
+    void point_update(int i, T1 x)
+    {
+        while (i < n)
+        {
+            fenwick[i] = f(fenwick[i], x);
             i += i & -i;
         }
     }
 
+    void point_update_0indexed(int i, T1 x)
+    {
+        return point_update(i + 1, x);
+    }
 };
 
-
-int main(){
-    vector<long long> arr = {1,2,3,4,5,6};
+int main()
+{
+    vector<long long> arr = {1, 2, 3, 4, 5, 6};
     Fenwick_Tree<long long> FT = Fenwick_Tree<long long>(arr);
-    cout << FT.range_query(6) << endl; 
+    cout << FT.range_query(1, 4) << endl;
 }
