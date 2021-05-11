6f7a9b379019f9b5a3af060dfd01f808251be7fc
decimalpack <decimalpack@users.noreply.github.com>
2021-05-11 16:48:56 +0530

    Removed redundant tests
    Removed:
     - Increment +1
     - Decrement -1
     - No change
    
    The above tests are redundant as they lie in the range [-100, 100],
    which is covered by test "From -100 to 100"

--- tests/fenwick_PQRU.cpp
+++ tests/fenwick_PQRU.cpp
@@ -11,32 +11,6 @@ TEST_CASE("Entire array")
 	iota(arr.begin(), arr.end(), 1);
 	Fenwick_Tree_PQRU<int> ft(arr);
 
-	SECTION("Increment +1")
-	{
-		ft.range_update(1, n, 1);
-		for (int i = 0; i < n; i++)
-		{
-			REQUIRE(ft.point_query(i + 1) == arr[i] + 1);
-		}
-	}
-	SECTION("Decrement -1")
-	{
-		ft.range_update(1, n, -1);
-		for (int i = 0; i < n; i++)
-		{
-			REQUIRE(ft.point_query(i + 1) == arr[i] - 1);
-		}
-	}
-
-	SECTION("No change")
-	{
-		ft.range_update(1, n, 0);
-		for (int i = 0; i < n; i++)
-		{
-			REQUIRE(ft.point_query(i + 1) == arr[i]);
-		}
-	}
-
 	SECTION("From -100 to 100")
 	{
 		int total = 0;
