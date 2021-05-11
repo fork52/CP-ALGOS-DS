b9cf4a8e7770882655bf93185730051a7451dbcb
decimalpack <decimalpack@users.noreply.github.com>
2021-05-11 15:36:59 +0530

    Add benchmark and subarray test for fenwick tree

--- tests/fenwick_PQRU.cpp
+++ tests/fenwick_PQRU.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
 #include "../Advanced Data Structures/Fenwick Trees/fenwick_PQRU.cpp"
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
 #include "catch.hpp"
 using namespace std;
 
-TEST_CASE("Operations on entire array")
+TEST_CASE("Entire array")
 {
 	const int n = 1000;
 	vector<int> arr(n);
@@ -46,6 +47,60 @@ TEST_CASE("Operations on entire array")
 			for (int i = 0; i < n; i++)
 			{
 				REQUIRE(ft.point_query(i + 1) == arr[i] + total);
+			}
+		}
+	}
+}
+TEST_CASE("Benchmark", "[.benchmark]")
+{
+	for (int n = 1; n <= 1e5; n *= 10)
+	{
+		vector<int> arr(n);
+		iota(arr.begin(), arr.end(), 1);
+		BENCHMARK("Array size: " + to_string(n))
+		{
+			CAPTURE(arr);
+			Fenwick_Tree_PQRU<int> ft(arr);
+			int total = 0;
+			for (int delta = -5; delta <= 5; delta++)
+			{
+				ft.range_update(1, n, delta);
+				total += delta;
+				for (int i = 0; i < n; i++)
+				{
+					REQUIRE(ft.point_query(i + 1) == arr[i] + total);
+				}
+			}
+		};
+	}
+}
+
+TEST_CASE("Subarray")
+{
+
+	const int n = 25;
+	vector<int> arr(n);
+	iota(arr.begin(), arr.end(), 1);
+	Fenwick_Tree_PQRU<int> ft(arr);
+
+	for (int l = 1; l <= n; l++)
+	{
+		for (int r = l; r <= n; r++)
+		{
+			stringstream test_name;
+			test_name << "Subarray: [" << l << ", " << r << "]";
+			SECTION(test_name.str())
+			{
+				int total = 0;
+				for (int delta = -10; delta <= 10; delta++)
+				{
+					ft.range_update(l, r, delta);
+					total += delta;
+					for (int i = l; i <= r; i++)
+					{
+						REQUIRE(ft.point_query(i) == arr[i - 1] + total);
+					}
+				}
 			}
 		}
 	}
