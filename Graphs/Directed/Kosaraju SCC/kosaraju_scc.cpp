e03168caa6ff846e388fce94cc29a3d12e53a9d3
fork52 <mrunankmistry52@gmail.com>
2021-11-07 13:23:30 +0530

    Added function for generating condensation_graph for Kosaraju algorithm.

--- Graphs/Directed/Kosaraju SCC/kosaraju_scc.cpp
+++ Graphs/Directed/Kosaraju SCC/kosaraju_scc.cpp
@@ -13,17 +13,24 @@ void disp_vec(vector<T1> arr){
     cout << "\n";
 }
 
+/*
+References:
+    https://cp-algorithms.com/graph/strongly-connected-components.html
+    https://youtu.be/QtdE7QPsWiU
+*/
 
 template<typename T1>
 class Kosaraju_SCC {
 public:
 
     ll n;
-    vector<vector<T1>> graph;
+    vector<vector<T1>> graph, rev_graph;
     stack<T1> st;
     vector<vector<T1>> components;
-    vector<vector<T1>> rev_graph;
     vector<bool> vis;
+
+    vector<T1> node_to_sccRoot;             // For mapping to node to scc Reperesntative.
+    vector<vector<T1>> condensation_graph;
 
     Kosaraju_SCC(vector<vector<T1>> &adj_list){
         n = adj_list.size();
@@ -38,10 +45,10 @@ public:
                 _dfs1(nei);
             }
         }
-
         st.push(node);
     }
 
+    // Step 2: Create the rev copy of the graph
     void _create_reverse_graph(){
         rev_graph = vector<vector<T1>>(n);
 
@@ -52,7 +59,7 @@ public:
         }
     }
 
-    // STEP 2: DFS for finding the components
+    // Step 3: DFS for finding the components
     void _dfs2(T1 node, int comp_ind){
         vis[node] = true;
         components[comp_ind].push_back(node);
@@ -66,23 +73,24 @@ public:
 
     /*
         Finds the Strongly connected components in the directed graph.
-        Stores the compenents in the 2D vector components
+        Stores the components in the 2D vector `components`.
     */
     void find_SCCs(){
         
         vis = vector<bool>(n, false);
-        // Random order dfs
+
+        // Random order dfs on the graph
         for(T1 node = 0; node < n; node++){
             if(vis[node] == false){
                 _dfs1(node);
             }
         }
 
-        // Reverse the graph
+        // Create a reversed/transposed copy the graph `rev_graph`
         _create_reverse_graph();
         fill(vis.begin(), vis.end(), false);
 
-        // Perform dfs again in order present in stack to find SCCs
+        // Perform dfs again in the order present in stack to find SCCs
         int component_index = 0; 
         while(!st.empty()){
             T1 node = st.top(); st.pop();
@@ -93,25 +101,68 @@ public:
             }
         }
     }
+    
+    /*
+        Call find_SCCs first before generating the condensation graph.
+        It is guaranteed that the condensation graph will be a DAG.
+    */
+    void generate_condensation_graph(){
+        int no_of_components = components.size();
+        node_to_sccRoot = vector<T1>(n);
+
+        condensation_graph = vector<vector<T1>>(no_of_components);
+
+        for(int c = 0; c < no_of_components; c++){
+            for(auto node : components[c]){
+                node_to_sccRoot[node] = c;
+            }
+        }
+
+        for(int node = 0; node < n; node++){
+            int sccRoot1 = node_to_sccRoot[node];
+            for(auto nei : graph[node]){
+                int sccRoot2 = node_to_sccRoot[nei];
+                if(sccRoot1 != sccRoot2){
+                    condensation_graph[sccRoot1].push_back(sccRoot2);
+                }
+            }
+        }
+    }
+
 };
 
 int main(){
-    vector<vector<int>> graph = {
+
+    vector<vector<int>> sample_graph = {
         {1},
         {2},
         {0, 4},
         {4},
-        {5},
+        {5, 6},
         {3},
         {7},
         {6}
     };
 
-    Kosaraju_SCC<int> obj(graph);
+    Kosaraju_SCC<int> obj(sample_graph);
+
+    // Findin the SCCs
     obj.find_SCCs();
 
+    // Generating the condensation graph
+    obj.generate_condensation_graph();
+
+    // Printing out the components in each SCC of the graph
+    cout << "No of components: " << obj.components.size() << "\n";
     for(int c = 0; c < obj.components.size(); c++){
         disp_vec(obj.components[c]);
     }
+
+    // Printing out the condensation graph. It is guaranteeed to be a DAG.
+    cout << "\nCondensation graph : \n";
+    for(int c = 0; c < obj.condensation_graph.size(); c++){
+        cout << c << " : "; disp_vec(obj.condensation_graph[c]);
+    }   
+
 
 }
