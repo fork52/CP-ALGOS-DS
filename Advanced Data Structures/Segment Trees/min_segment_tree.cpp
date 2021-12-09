c193fa9a3087c0d663fb717658b170b80cf4121a
fork52 <mrunankmistry52@gmail.com>
2021-12-09 19:31:05 +0530

    Added min and sum segment trees

--- Advanced Data Structures/Segment Trees/min_segment_tree.cpp
+++ Advanced Data Structures/Segment Trees/min_segment_tree.cpp
@@ -24,6 +24,7 @@ Related Problems:
 template<typename T1>
 class MinSegmentTree{
 public:
+    static const ll identity = INT64_MAX;
     T1 nodeLeft, nodeRight;
     MinSegmentTree *leftChild, *rightChild;
     T1 nodeMin;
@@ -50,10 +51,10 @@ public:
         return this -> nodeLeft == this -> nodeRight;
     }
 
-    /* Recalculates the sum of current node using left and right child sums*/
+    /* Recalculates the min for current node using left and right child mins*/
     void recalculate(){
         if( isLeaf() ) return;
-        nodeMin = leftChild -> nodeMin + rightChild -> nodeMin;
+        nodeMin = min(leftChild -> nodeMin, rightChild -> nodeMin);
     }
 
     void pointUpdate(int index, int newVal){
@@ -73,31 +74,31 @@ public:
 
     T1 rangeSum(int query_low, int query_high){
         if(query_high < nodeLeft || query_low > nodeRight){  //disjoint
-            return 0;
+            return identity;
         }
         else if(query_low <= nodeLeft && query_high >= nodeRight  ){  // complete cover
             return nodeMin;
         }
         else{
-            return leftChild -> rangeSum(query_low, query_high) + rightChild -> rangeSum(query_low , query_high);
+            return min(leftChild -> rangeSum(query_low, query_high), rightChild -> rangeSum(query_low , query_high));
         }
     }
 };
 
 int main(){
-    vector<ll> arr = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+    vector<ll> arr = {10, 15, 1, 5, 7, 9, 10, 20, 120};
     ll n = arr.size();
 
     //Build the segment tree
     MinSegmentTree segTree(0ll, n - 1, arr); 
 
     //Perform range query
-    cout << "Sum from 1 to 5: " << segTree.rangeSum(1, 5) << "\n";
+    cout << "Min from 1 to 5: " << segTree.rangeSum(1, 5) << "\n";
     cout << "Sum from 0 to 3: " << segTree.rangeSum(0, 3) << "\n";
 
     cout << "\n";
     //pointupdate
-    segTree.pointUpdate(1, 100);
+    segTree.pointUpdate(2, 100);
 
     //Perform range query
     cout << "Sum from 1 to 5: " << segTree.rangeSum(1, 5) << "\n";
