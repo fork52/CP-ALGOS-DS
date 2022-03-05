e4c0baab863a601e266aed697d9e5cf427dbf769
fork52 <mrunankmistry52@gmail.com>
2022-03-05 17:40:16 +0530

    Added binary exponentiation.

---
+++ Number Theory/binary_exp.cpp
@@ -0,0 +1,16 @@
+using ll = long long;
+
+/*
+    Computes a ^ b under mod m 
+*/
+ll binpow(ll a, ll b, ll m) {
+    a %= m;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
