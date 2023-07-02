b9e45167c005c13777bd72a0586ac702551e9371
fork52 <mrunankmistry52@gmail.com>
2023-07-02 17:46:34 +0530

    Add prime test for sieve factorization.

--- tests/Number-Theory/test_sieve_factorization.cpp
+++ tests/Number-Theory/test_sieve_factorization.cpp
@@ -3,10 +3,22 @@
 
 using ll = long long;
 
-// Test prime_sieve Small
+// Test sieve factorize Small
 TEST(SieveFactorization, SmallTest) {
     Factorizer obj(1000);
 
     vector<ll> facts = obj.prime_factorize_simple(12);
+    vector<ll> expected = {2, 2, 3};
     EXPECT_EQ(facts.size(), 3);
+    EXPECT_EQ(facts, expected);
+}
+
+// Test sieve factorize prime
+TEST(SieveFactorization, PrimeNumberTest) {
+    Factorizer obj(1000);
+
+    vector<ll> facts = obj.prime_factorize_simple(19);
+    vector<ll> expected = {19};
+    EXPECT_EQ(facts.size(), 1);
+    EXPECT_EQ(facts, expected);
 }
