04f8846a278bf53f667b8ee857664dacef5253db
fork52 <mrunankmistry52@gmail.com>
2023-12-10 22:02:07 +0530

    Add delete methods

--- Trees/Tries/Tries.cpp
+++ Trees/Tries/Tries.cpp
@@ -1,114 +1,171 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 // Reference: https://leetcode.com/problems/implement-trie-prefix-tree/
 
 // Assumes that you only have lower-case Alphabets
-class Trie{
-    private:
-        Trie * children[26] = {};
-        bool isword = false;
-
-    public:
-
-        Trie(){}
-        
-        /** Inserts a word into the trie. */
-        void insert(string word) {
-            Trie *cur = this;
-            for(auto &l : word){
-                if(cur->children[l-'a'] == nullptr){
-                    cur->children[l-'a'] = new Trie();
-                }
-                cur = cur->children[l-'a'];
-            }
-            cur->isword = true;
-        }
-        
-        /** Returns if the word is in the trie. */
-        bool search(string word) {
-            Trie *cur = this;
-            for(auto &l : word){
-                if(cur->children[l-'a'] == nullptr){
-                    return false;
-                }
-                cur = cur->children[l-'a'];
-            }
-            return cur && cur->isword;
-
-        }
-        
-        /** Returns if there is any word in the trie that starts with the given prefix. */
-        bool startsWith(string prefix) {
-            Trie *cur = this;
-            for(auto &l : prefix){
-                if(cur->children[l-'a'] == nullptr){
-                    return false;
-                }
-                cur = cur->children[l-'a'];
-            }
-            return cur;
-        }
-};
-
-/* Will work for all ascii strings */
-class Generic_Trie {
-
-    map<char, Generic_Trie *> children = {};
+class Trie
+{
+private:
+    Trie *children[26] = {};
     bool isword = false;
 
 public:
-    /** Initialize your data structure here. */
-    Generic_Trie() {}
-    
+    Trie() {}
+
     /** Inserts a word into the trie. */
-    void insert(string word) {
-        Generic_Trie *cur = this;
-        for(auto &l : word){
-            if(!cur->children.count(l-'a')){
-                  cur->children[l-'a'] = new Generic_Trie();
+    void insert(string word)
+    {
+        Trie *cur = this;
+        for (auto &l : word)
+        {
+            if (cur->children[l - 'a'] == nullptr)
+            {
+                cur->children[l - 'a'] = new Trie();
             }
-            cur = cur->children[l-'a'];
+            cur = cur->children[l - 'a'];
         }
         cur->isword = true;
     }
-    
+
     /** Returns if the word is in the trie. */
-    bool search(string word) {
-        Generic_Trie *cur = this;
-        for(auto &l : word){
-            if(!cur->children.count(l-'a')){
+    bool search(string word)
+    {
+        Trie *cur = this;
+        for (auto &l : word)
+        {
+            if (cur->children[l - 'a'] == nullptr)
+            {
                 return false;
             }
-            cur = cur->children[l-'a'];
+            cur = cur->children[l - 'a'];
         }
         return cur && cur->isword;
     }
-    
+
     /** Returns if there is any word in the trie that starts with the given prefix. */
-    bool startsWith(string prefix) {
-        Generic_Trie *cur = this;
-        for(auto &l : prefix){
-            if(!cur->children.count(l-'a')){
+    bool startsWith(string prefix)
+    {
+        Trie *cur = this;
+        for (auto &l : prefix)
+        {
+            if (cur->children[l - 'a'] == nullptr)
+            {
                 return false;
             }
-            cur = cur->children[l-'a'];
+            cur = cur->children[l - 'a'];
         }
         return cur;
     }
+
+    bool deleteWord(string &word, int index = 0)
+    {
+        if (index == word.length())
+        {
+            if (!this->isword)
+                return false; // Word doesn't exist
+            this->isword = false;
+            return isEmptyNode();
+        }
+
+        int chIndex = word[index] - 'a';
+        if (this->children[chIndex] == nullptr)
+            return false; // Word doesn't exist
+
+        bool canDelete = this->children[chIndex]->deleteWord(word, index + 1);
+        if (canDelete)
+        {
+            delete this->children[chIndex];
+            this->children[chIndex] = nullptr;
+            return isEmptyNode() && !this->isword;
+        }
+        return false;
+    }
+
+    bool isEmptyNode()
+    {
+        for (int i = 0; i < 26; i++)
+        {
+            if (this->children[i])
+                return false;
+        }
+        return true;
+    }
 };
 
+/* Will work for all ascii strings */
+// class Generic_Trie
+// {
 
-int main(){
+//     map<char, Generic_Trie *> children = {};
+//     bool isword = false;
 
-    //Usage of the Trie
-    Generic_Trie root = Generic_Trie();
-    // Trie root = Trie();
+// public:
+//     /** Initialize your data structure here. */
+//     Generic_Trie() {}
 
-    root.insert("apple");
+//     /** Inserts a word into the trie. */
+//     void insert(string word)
+//     {
+//         Generic_Trie *cur = this;
+//         for (auto &l : word)
+//         {
+//             if (!cur->children.count(l - 'a'))
+//             {
+//                 cur->children[l - 'a'] = new Generic_Trie();
+//             }
+//             cur = cur->children[l - 'a'];
+//         }
+//         cur->isword = true;
+//     }
 
-    cout << root.search("apple") << endl;
-    cout << root.startsWith("app") << endl;
-    cout << root.startsWith("apk") << endl;
+//     /** Returns if the word is in the trie. */
+//     bool search(string word)
+//     {
+//         Generic_Trie *cur = this;
+//         for (auto &l : word)
+//         {
+//             if (!cur->children.count(l - 'a'))
+//             {
+//                 return false;
+//             }
+//             cur = cur->children[l - 'a'];
+//         }
+//         return cur && cur->isword;
+//     }
 
+//     /** Returns if there is any word in the trie that starts with the given prefix. */
+//     bool startsWith(string prefix)
+//     {
+//         Generic_Trie *cur = this;
+//         for (auto &l : prefix)
+//         {
+//             if (!cur->children.count(l - 'a'))
+//             {
+//                 return false;
+//             }
+//             cur = cur->children[l - 'a'];
+//         }
+//         return cur;
+//     }
+// };
+
+int main()
+{
+
+    // Usage of the Trie
+    Trie root = Trie();
+
+    string apple = "a";
+    string applet = "ab";
+    root.insert(apple);
+    root.insert(applet);
+
+    cout << root.startsWith(apple) << endl;
+    cout << root.startsWith(applet) << endl;
+
+    root.deleteWord(apple, 0);
+
+    cout << root.search(apple) << endl;
+    cout << root.search(applet) << endl;
 }
