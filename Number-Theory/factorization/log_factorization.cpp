a714385aa7f9ea1ba6d996cf474f74fe090186bf
fork52 <mrunankmistry52@gmail.com>
2024-02-26 20:49:09 +0530

    Add getAllDivisors and prime_factorize methods

--- Number-Theory/factorization/log_factorization.cpp
+++ Number-Theory/factorization/log_factorization.cpp
@@ -1,11 +1,34 @@
 #include <vector>
 #include <numeric>
 #include <iostream>
+#include <algorithm>
 
 class LogFactorizer
 {
     long long n;
     std::vector<long long> smallestPrimeFactor;
+    std::vector<long long> _primeFactors;
+    std::vector<std::pair<long long, long long>> _divisors;
+
+    void _findDivisors(long long i, long long num)
+    {
+        if (i == (long long)_divisors.size())
+        {
+            _primeFactors.push_back(num);
+            return;
+        }
+
+        // Ignore
+        _findDivisors(i + 1, num);
+
+        // Take
+        auto [prime_num, freq] = _divisors[i];
+        for (int mul = 1; mul <= freq; mul++)
+        {
+            num *= prime_num;
+            _findDivisors(i + 1, num);
+        }
+    }
 
     void init()
     {
@@ -36,7 +59,10 @@ public:
         this->init();
     }
 
-    std::vector<long long> factorize(long long x)
+    /*
+       Returns the prime factorization of a number in the sorted order
+   */
+    std::vector<long long> factorize_simple(long long x)
     {
         std::vector<long long> primeFactors;
         while (x > 1)
@@ -47,12 +73,48 @@ public:
         return primeFactors;
     }
 
-    bool isPrime(long long x){
+    /*
+        Returns the prime factorization of a number in the form:
+        (prime1, cnt1), (prime2, cnt2) ....
+    */
+    std::vector<std::pair<long long, long long>> prime_factorize(long long x)
+    {
+        std::vector<std::pair<long long, long long>> primeFactors;
+        long long pre = -1;
+        while (x > 1)
+        {
+            long long factor = this->smallestPrimeFactor[x];
+            if(factor == pre){
+                primeFactors.back().second += 1;
+            }
+            else{
+                primeFactors.emplace_back(factor, 1);
+            }
+            pre = factor;
+            x = x / factor;
+        }
+        return primeFactors;
+    }
+
+    bool isPrime(long long x)
+    {
         return (x != 1) && (this->smallestPrimeFactor[x] == x);
     }
+
+    std::vector<long long> getAllDivisors(long long x)
+    {
+        _divisors = this->prime_factorize(x);
+
+        _findDivisors(0, 1);
+        std::vector<long long> primeFactors = _primeFactors;
+        std::sort(primeFactors.begin(), primeFactors.end());
+
+        // Reset vectors
+        _primeFactors.clear(); _divisors.clear();
+
+        return primeFactors;
+    }
 };
-
-
 // int main(){
 //     Factorizer obj(10);
 //     std::vector<long long> facts = obj.factorize(10);
--- tests/Number-Theory/test_log_factorization.cpp
+++ tests/Number-Theory/test_log_factorization.cpp
@@ -4,36 +4,55 @@
 
 TEST(LogFactorization, SmallTest5) {
     LogFactorizer obj(10);
-    std::vector<long long> facts = obj.factorize(5);
+    std::vector<long long> facts = obj.factorize_simple(5);
     std::vector<long long> expected = {5};
     EXPECT_EQ(facts, expected);
 }
 
 TEST(LogFactorization, TEST10) {
     LogFactorizer obj(10);
-    std::vector<long long> facts = obj.factorize(10);
+    std::vector<long long> facts = obj.factorize_simple(10);
     std::vector<long long> expected = {2, 5};
     EXPECT_EQ(facts, expected);
 }
 
 TEST(LogFactorization, SmallTest) {
     LogFactorizer obj(10);
-    std::vector<long long> facts = obj.factorize(5);
+    std::vector<long long> facts = obj.factorize_simple(5);
     std::vector<long long> expected = {5};
     EXPECT_EQ(facts, expected);
 }
 
 TEST(LogFactorization, PrimeNumberTest) {
     LogFactorizer obj(19);
-    std::vector<long long> facts = obj.factorize(19);
+    std::vector<long long> facts = obj.factorize_simple(19);
     std::vector<long long> expected = {19};
     EXPECT_EQ(facts, expected);
 }
 
 TEST(LogFactorization, BasicTest) {
     LogFactorizer obj(10);
-    std::vector<long long> facts = obj.factorize(2);
+    std::vector<long long> facts = obj.factorize_simple(2);
     std::sort(facts.begin(), facts.end());
     std::vector<long long> expected = {2};
     EXPECT_EQ(facts, expected);
 }
+
+TEST(LogFactorization, TestPrimeFreq) {
+    LogFactorizer obj(120);
+    std::vector<std::pair<long long, long long>> facts = obj.prime_factorize(120);
+    std::sort(facts.begin(), facts.end());
+    std::vector<std::pair<long long, long long>> expected = {
+        {2, 3}, {3, 1}, {5, 1}
+    };
+    EXPECT_EQ(facts, expected);
+}
+
+TEST(LogFactorization, TestDivisors) {
+    LogFactorizer obj(120);
+    std::vector<long long> facts = obj.getAllDivisors(120);
+    std::vector<long long> expected = {1, 2, 3, 4, 5, 6, 8, 10, 12, 15, 20, 24, 30, 40, 60, 120};
+    EXPECT_EQ(facts, expected);
+}
+
+
